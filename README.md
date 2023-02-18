5c97396c0470af632909657f2c38760c7f0b5ff7
Abel Casas <acasasgarcia1@cifpfbmoll.eu>
2023-02-18 15:27:45 +0100

    build(): add asserj dependency, test casses and main

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # arnold-enum-types
+
 Repository to take a first look at the enum types in Java. The exercise consist in calculating the weight of something in each of the planets in our Solar System 
+
+Initial statment: [dfleta](https://github.com/dfleta/arnold-enum-type)
+
+Solution of my teacher: [solution](https://github.com/dfleta/Java/tree/master/arnoldEnumTypeMaven)
--- arnoldenumtypes/src/test/java/edu/craptocraft/arnoldenumtypes/AppTest.java
+++ arnoldenumtypes/src/test/java/edu/craptocraft/arnoldenumtypes/AppTest.java
@@ -7,14 +7,22 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void planetaConstructorTest() {
+        // voy a asegurarme de que los metodos de los Enum Types
+        // se comportan como yo espero
+        Planeta planeta = Planeta.MERCURY;
+        assertThat(planeta).isInstanceOf(Planeta.class);
+        assertThat(planeta.ordinal()).isZero();
+        assertThat(planeta.name()).isEqualToIgnoringCase("MERCURY");
+        assertThat(Planeta.valueOf(planeta.name())).isEqualTo(Planeta.MERCURY);
+        assertThat(planeta.compareTo(Planeta.MERCURY)).isZero();
+        assertThat(planeta.toString()).isEqualToIgnoringCase("MERCURY");
+        assertThat(planeta.equals(Planeta.MERCURY)).isTrue();
+        assertThat(Planeta.values()[0]).isEqualTo(planeta);
     }
 }
