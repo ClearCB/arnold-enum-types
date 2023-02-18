c74dafa3bc81aae5b5502899a8dcd917c79b3430
Abel Casas <acasasgarcia1@cifpfbmoll.eu>
2023-02-18 15:58:04 +0100

    feat(constructopr/getRadio/getMasa): añadir ambos getters y el constructor

--- arnoldenumtypes/src/test/java/edu/craptocraft/arnoldenumtypes/AppTest.java
+++ arnoldenumtypes/src/test/java/edu/craptocraft/arnoldenumtypes/AppTest.java
@@ -1,28 +1,52 @@
 package edu.craptocraft.arnoldenumtypes;
 
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
+    
+	public static String[] planetas;
+
+	@BeforeClass
+	public static void creacionArrayPlanetasSetup() {
+		planetas = new String[8];
+		int planetasIncluidos = 0;
+		for (Planeta planeta : Planeta.values()) {
+			planetas[planeta.ordinal()] = planeta.name();
+			planetasIncluidos += 1;
+		}
+		assertThat(planetasIncluidos).isEqualTo(Planeta.values().length);
+		assertThat(planetas).doesNotContainNull();
+	}
+
+	@Test
+	public void planetaConstructorTest() {
+		// voy a asegurarme de que los metodos de los Enum Types
+		// se comportan como yo espero
+		Planeta planeta = Planeta.MERCURY;
+		assertThat(planeta).isInstanceOf(Planeta.class);
+		assertThat(planeta.ordinal()).isZero();
+		assertThat(planeta.name()).isEqualToIgnoringCase("MERCURY");
+		assertThat(Planeta.valueOf(planeta.name())).isEqualTo(Planeta.MERCURY);
+		assertThat(planeta).isEqualByComparingTo(Planeta.MERCURY);
+		assertThat(planeta.toString()).isEqualToIgnoringCase("MERCURY");
+		assertThat(planeta).isEqualTo(Planeta.MERCURY);
+		assertThat(Planeta.values()[0]).isEqualTo(planeta);
+	}
+
+    
+	@Test
+	public void planetaGetMasaTest() {
+		Planeta planeta = Planeta.MERCURY;
+		assertThat(planeta.getMasa()).isEqualTo(3.303e+23);
+	}
+
     @Test
-    public void planetaConstructorTest() {
-        // voy a asegurarme de que los metodos de los Enum Types
-        // se comportan como yo espero
-        Planeta planeta = Planeta.MERCURY;
-        assertThat(planeta).isInstanceOf(Planeta.class);
-        assertThat(planeta.ordinal()).isZero();
-        assertThat(planeta.name()).isEqualToIgnoringCase("MERCURY");
-        assertThat(Planeta.valueOf(planeta.name())).isEqualTo(Planeta.MERCURY);
-        assertThat(planeta.compareTo(Planeta.MERCURY)).isZero();
-        assertThat(planeta.toString()).isEqualToIgnoringCase("MERCURY");
-        assertThat(planeta.equals(Planeta.MERCURY)).isTrue();
-        assertThat(Planeta.values()[0]).isEqualTo(planeta);
-    }
+	public void PlanetaGetRadioTest() {
+		Planeta planeta = Planeta.MERCURY;
+		assertThat(planeta.getRadio()).isEqualTo(2.4397e+6);
+	}
+
 }
